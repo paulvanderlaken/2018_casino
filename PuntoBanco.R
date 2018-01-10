4c3e593f99c135a1e83e59f67407feb6876fd69e
paulvanderlaken <paulvanderlaken@gmail.com>
2018-01-10 15:33:54 +0100

    Slight modifications to visualizations

--- PuntoBanco.R
+++ PuntoBanco.R
@@ -4,13 +4,15 @@
 ### PAULVANDERLAKEN.COM
 
 # load in packages
-pkg <- c("here", "tidyverse")
+pkg <- c("here", "tidyverse", "extrafont")
 sapply(pkg, function(x){
   if(!x %in% installed.packages()) install.packages(x)
   library(x, character.only = TRUE)
   return(x)
 })
-
+# load in fonts
+loadfonts(device = "win")
+font <- "Lucida Sans Unicode"
 
 # deck specifications
 suits <- c("hearts", "diamonds", "clubs", "spades")
@@ -19,18 +21,19 @@ values <- c(seq(2,9), 10, 10, 10, 10, 1)
 ranks_values <- setNames(values, ranks)
 deck_size <- length(ranks) * length(suits)
 number_of_decks <- 6
+  
+# specify simulation settings
+simulations <- 50000
+hands_per_simulation <- 100
+bet_amount <- 100
+egalite_payout_mult <- 9
 
 # outcomes
 outcome_punto <- "Punto"
 outcome_banco <- "Banco"
 outcome_banco5 <- "Banco5"
 outcome_egalite <- "Egalité"
-  
-# specify simulation settings
-simulations <- 50
-hands_per_simulation <- 100
-bet_amount <- 100
-egalite_payout_mult <- 8
+egalite_label <- paste0(outcome_egalite, " (", egalite_payout_mult, "-1)")
 
 
 # create empty matrix
@@ -83,10 +86,14 @@ print_hand_value <- function(name, hand){
 # draw cards from deck to hand
 # and remove cards from deck
 deal_cards <- function(deck, hand, n_cards = 1){
+  # check if there are enough cards 
   if(n_cards < 1) stop("Must deal more than 1 card")
   if(n_cards > length(deck)) stop("Not enough cards in deck")
+  # add drawn cards to hand
   hand <- c(hand, deck[seq(n_cards)])
+  # remove drawn card form deck
   assign(as.character(substitute(deck)), deck[-1:-n_cards], parent.frame())
+  
   return(hand)
 }
 
@@ -200,12 +207,13 @@ for(s in seq(simulations)){
     results[row_nr, ] <- c(s, h, game_results$result)
     
   }
-  
+  # print progress
+  if((s %% 100) == 0) print(s)
 }
 
 compute_payoff <- function(result, bet){
   # result == NA
-  if(is.na(bet)) return(payoff <- 0)
+  if(is.na(bet)) return(payoff <- NA)
   if(bet == outcome_banco5) bet <- outcome_banco
   # result == egalite
   if(result == outcome_egalite){
@@ -222,7 +230,7 @@ compute_payoff <- function(result, bet){
     if(bet == outcome_egalite) payoff <- -bet_amount
     else if(bet == outcome_punto) payoff <- -bet_amount
     else if(bet == outcome_banco & result == outcome_banco) payoff <- bet_amount
-    else if(bet == outcome_banco & result == outcome_banco5) payoff <- bet_amount/2
+    else if(bet == outcome_banco & result == outcome_banco5) payoff <- bet_amount / 2
   }
   return(payoff)
 }
@@ -233,31 +241,71 @@ results %>%
   as.tibble() %>%
   mutate(Simulation = as.numeric(Simulation)) %>%
   mutate(Hand = as.numeric(Hand)) %>%
-  mutate(Egalité = ifelse(Result == outcome_egalite, bet_amount * egalite_payout_mult, -bet_amount)) %>%
+  # compute profit for simple strategies
+  mutate(Egalité = map2_dbl(Result, outcome_egalite, ~ compute_payoff(.x, .y))) %>%
   mutate(Punto = map2_dbl(Result, outcome_punto, ~ compute_payoff(.x, .y))) %>%
   mutate(Banco = map2_dbl(Result, outcome_banco, ~ compute_payoff(.x, .y))) %>%
+  # compute profit for last hand strategies
   group_by(Simulation) %>%
-  mutate(lagResult1 = lag(Result, 1)) %>%
+  mutate(lagResult1 = lag(Result, 1)) %>% # lag last hand
   mutate(LastHand = map2_dbl(Result, lagResult1, ~ compute_payoff(.x, .y))) %>%
   mutate(LastHand_PB = ifelse(lagResult1 == outcome_egalite | is.na(lagResult1), 
-                             0, LastHand)) %>%
-  select(-lagResult1) %>%
+                             NA, LastHand)) %>%
+  select(-lagResult1) %>% # remove last hand
+  # dataframe to long format
   gather(Strategy, Profit, -Simulation, -Hand, -Result) %>%
-  mutate(Strategy = factor(Strategy, levels = c(outcome_punto, outcome_banco, outcome_egalite,
-                                                "LastHand", "LastHand_PB"))) %>%
+  # specify order of strategies
+  mutate(Strategy = factor(Strategy, 
+                           levels = c(outcome_punto, outcome_banco, outcome_egalite,
+                                      "LastHand", "LastHand_PB"),
+                           labels = c(outcome_punto, outcome_banco, egalite_label,
+                                     "LastHand", "LastHand_PB"))) %>%
+  # some strategies don't play each hand
   group_by(Simulation, Strategy) %>%
-  mutate(Profit = cumsum(Profit)) ->
+  mutate(HandPlayed = cumsum(!is.na(Profit))) %>%
+  # compute cumulative profit
+  mutate(Profit = cumsum(ifelse(is.na(Profit), 0, Profit))) %>%
+  arrange(Simulation, Strategy, Hand) ->
   results_cum
 
 results_cum %>%
-  ggplot(aes(x = Hand, y = Profit, col = Strategy)) +
-  geom_hline(yintercept = 0, lty = "dotted") +
-  geom_hline(yintercept = -bet_amount, lty = "dotted", col = "red") +
+  filter(HandPlayed > 0) %>%
+  # filter(Simulation < 10) %>%
+  ggplot(aes(x = HandPlayed, y = Profit, col = Strategy)) +
+  geom_hline(yintercept = 0, lty = 5, col = "grey70", size = 1) +
+  geom_hline(yintercept = -bet_amount, lty = 5, col = "red", size = 1) +
+  geom_hline(yintercept = bet_amount, lty = 5, col = "green", size = 1) +
   # geom_line(aes(group = interaction(Simulation, Strategy)), alpha = 0.1) +
-  geom_smooth(aes(group = Strategy), method = "gam", size = 1) +
-  # scale_color_viridis_d() +
-  labs(y = "Profit (%)") +
-  coord_cartesian(ylim = c(-500, 500)) +
+  geom_smooth(aes(group = Strategy), method = "gam", size = 1.5, se = FALSE) +
+  annotate("label", x = hands_per_simulation/2, y = 0, label = "No profit/loss", 
+           label.size = NA, vjust = 0, hjust = 0.5, col = "grey70", fill = "transparent",
+           size = 3.5, family = font) +
+  annotate("label", x = 5, y = -bet_amount, label = "Lost bet amount", 
+           label.size = NA, vjust = 0, hjust = 0.5, col = "red", fill = "transparent",
+           size = 3.5, family = font) +
+  annotate("label", x = 5, y = bet_amount, label = "Won bet amount", 
+           label.size = NA, vjust = 1, hjust = 0.5, col = "green", fill = "transparent",
+           size = 3.5, family = font) +
+  scale_color_manual(values = c("red", "yellow2", "lightblue", "green3", "blue")) +
+  labs(y = "Average Profit (% of bet size)",
+       x = "Number of Hands Played",
+       title = "The House Always Wins At Baccarat (Punto Banco)",
+       subtitle = paste(
+         paste("Average payoff of", format(simulations, big.mark = ","), 
+               "simulated playthroughs of", hands_per_simulation, "hands of North American baccarat"),
+         "Every strategy results in losses. Best to bet only Banco and avoid Egalité completely (even 9-1).",
+         sep = "\n"),
+       caption = "github.com/paulvanderlaken/2018_casino   //   paulvanderlaken.com") +
+  coord_cartesian(ylim = c(-bet_amount*2, bet_amount)) +
   theme_light() +
-  theme(legend.position = "bottom")
-
+  theme(legend.position = c(0.5, 1), 
+        legend.justification = c(0.5, 1),
+        legend.background = element_rect(colour = "grey70", size = rel(0.5)),
+        legend.direction = "horizontal",
+        text = element_text(family = font),
+        plot.title = element_text(size = 20))
+ggsave(here(paste0("images/average_payoff_PB_",
+                   simulations, "s",
+                   hands_per_simulation, "h",
+                   ".png")),
+       dpi = 600, width = 9, height = 6)
